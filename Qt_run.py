cc270c799e45f4af037a201ba754ea6d3a8649a9
ekfkdlxm <ekfkdlxm@gmail.com>
2018-11-27 17:08:27 +0900

    PyQt-mortarCount

--- Qt_run.py
+++ Qt_run.py
@@ -1,3 +1,11 @@
+import sys
+import Object_detection_image as objectDetection
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+
+
 import numpy as np
 import cv2
 import math
@@ -5,13 +13,9 @@ import pymssql
 import time
 import datetime
 import tensorflow as tf
-import multiprocessing
 
-import Object_detection_image as objectDetection
 
-#  데이터 베이스 연결
-
-conn= pymssql.connect(host='192.168.4.31', user = 'sa' ,password= 'auto',database = 'PCS',  as_dict=True)
+conn= pymssql.connect(host='172.17.22.6', user = 'sa' ,password= 'sam@pyo#123',database = 'PCS',  as_dict=True)
 
 #gpu 메모리 해결책
 
@@ -25,51 +29,30 @@ ipAddress = "rtsp://admin:sampyo!1@10.62.67.226:554/profile2/media.smp"
 
 cursor = conn.cursor()
 cursor2 = conn.cursor()
-
-#  쿼리 테스트
-#cursor.execute("INSERT INTO [test1].[dbo].[PCS_PRODUCT_COUNT] (PLANT_CD,BPNO,ITEM_CD,PDATE,LINE_NO,PTIME,PMINUTE) VALUES ('I05','B','ABCD','2008',%d,'1254','1424')",(mortarCount+10))
-#conn.commit()
-
-
-
-#  영상 실행
-cap = cv2.VideoCapture(ipAddress)
-#cap = cv2.VideoCapture("7.mov")
-i = 0
-
-# RGB색상 상수
-SCALAR_BLACK = (0,0,0)
-SCALAR_WHITE = (255,255,255)
-SCALAR_BLUE = (255,0,0)
-SCALAR_GREEN = (0,200,0)
-SCALAR_RED = (0,0,255)
-
-
-
 global blobs
+
 blobs= []
 
+global firstFrame
+
+firstFrame = True
 # 몰탈 count
 mortarCount=np.zeros((6),np.int64)
 # 몰탈 DB정보 업데이트용 TEMP
 mortarTemp=np.zeros((6),np.int64)
 #class DB정보 업데이트용 TEMP
-classesTemp = '01'
+classesTemp = '101001140'
 
-#  첫프레임 read
-_, imgInput = cap.read()
-_, imgPre = cap.read()
 
-#  첫 objectDetection Test
-image_expanded = np.expand_dims(imgInput, axis=0)
-objectDetection.mortarClassification(imgInput,image_expanded)
-
-# 관심영역 Roi 지정
 
 
 passLine = False
 
-firstFrame = True
+SCALAR_BLACK = (0,0,0)
+SCALAR_WHITE = (255,255,255)
+SCALAR_BLUE = (255,0,0)
+SCALAR_GREEN = (0,200,0)
+SCALAR_RED = (0,0,255)
 
 #  객체의 정보를 담고있는 blob class
 class blob :
@@ -212,18 +195,10 @@ def drawBlobInfoOnImage(blobs,m1):
  ####################################################################################
 # 카운팅된 숫자를 실시간으로 영상 좌측상단에 표시한다.
 def drawMortarCountOnImage(mortarCount,image):
-    initText = "mortar Counter(1): " + str(mortarCount[0])
-    initText2 ="mortar Counter(2): " + str(mortarCount[1])
-    initText3 = "mortar Counter(3): " + str(mortarCount[2])
-
+    initText = "mortar Counter: " + str(mortarCount[0])
    # initText2 = "mortar Counter2"
     cv2.putText(image, " {}".format(initText), (10, 20),
         cv2.FONT_HERSHEY_SIMPLEX, 0.5, SCALAR_RED, 2)
-    cv2.putText(image, " {}".format(initText2), (10, 40),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.5, SCALAR_BLUE, 2)
-    cv2.putText(image, " {}".format(initText3), (10, 60),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.5, SCALAR_BLACK, 2)
-
   #  cv2.putText(image, " {}".format(initText2), (20, 30),cv2.FONT_HERSHEY_SIMPLEX,0.5 (0,255,255),2)
 
 ####################################################################################
@@ -252,17 +227,17 @@ def mortarClassificationAndCount(classes,mortarCount):
     if(classes == 1.0):
 
         mortarCount[0]+=1
-        classesTemp = '01'
+        classesTemp = '101001140'
 
     elif(classes == 2.0):
 
         mortarCount[1]+=1
-        classesTemp = '02'
+        classesTemp = '104001140'
 
     elif(classes == 3.0):
 
         mortarCount[2]+=1
-        classesTemp = '03'
+        classesTemp = ' '
 
     elif (classes == 4.0):
 
@@ -285,219 +260,272 @@ def mortarClassificationAndCount(classes,mortarCount):
 
 
 ####################################################################################
-# Main 부분
-while True:
 
+#form_class = uic.loadUiType("test.ui")[0]
+
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super(MyWindow,self).__init__()
+        #self.setupUi(self)
+        self.imgPre=None
+        self.imgInput=None
+        self.imgDiff=None
+        uic.loadUi('test.ui',self)
+        self.loadButton.clicked.connect(self.startV)
+        self.firstFrame = True
+        global blobs
+        blobs =[]
+        self.passLine = False
+
+
+# mortarCount를 UI로 display 해주는 함수
+    def mortarCounter(self):
+
+        self.mortar1.display(mortarCount[0])
+        self.mortar2.display(mortarCount[1])
+        self.mortar3.display(mortarCount[2])
+
+####################################################################################
+# 영상 시작 , 초기 변수 선언
+    def  startV(self):
+        self.capture=cv2.VideoCapture(ipAddress)
+        #self.capture=cv2.VideoCapture("7.mov")
+
+
+
+        ret,self.imgPre=self.capture.read()
+        rows, cols, channels = self.imgPre.shape
+
+
+
+        self.Line = np.zeros((2, 2), np.float32)
+        self.linePosition = round(cols * 0.75)
+        self.Line[0][0] = self.linePosition
+        self.Line[1][0] = self.linePosition
+        self.Line[0][1] = 0
+        self.Line[1][1] = cols
+
+
+        self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT,480)
+        self.capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+
+
+        self.timer=QTimer(self)
+        self.timer.timeout.connect(self.update_frame)
+        self.timer.start(10)
+
+####################################################################################
+# opencv 전처리 분류, DB 업데이트등 모든걸 하는 함수
+    def update_frame(self):
 
         # 현재 시간불러오기 시,분,초
-    now = time.gmtime(time.time())
-    # 현재 날짜와 시간 분을 9분늦게 DB 형식에 맞춰 문자열로 저장
-    plcTemp = datetime.datetime.now() - datetime.timedelta(minutes=9)
+        now = time.gmtime(time.time())
+        # 현재 날짜와 시간 분을 9분늦게 DB 형식에 맞춰 문자열로 저장
+        plcTemp = datetime.datetime.now() - datetime.timedelta(minutes=9)
 
-    plcTime = plcTemp.strftime("%Y-%m-%d %H:%M")
+        plcTime = plcTemp.strftime("%Y-%m-%d %H:%M")
 
-    # 프레임 read
-    _, imgPre=cap.read()
-    _, imgInput =cap.read()
 
+        ret,self.imgPre=self.capture.read()
+        ret,self.imgInput=self.capture.read()
 
+        #self.imgPre=cv2.flip(self.imgPre,1)
+        #self.imgInput = cv2.flip(self.imgInput, 1)
 
+        if not ret:
+            print('end video')
+            return
 
-    if not _:
-       print('end video')
-       break
+        if ret is True:
+            grayInput = cv2.cvtColor(self.imgInput, cv2.COLOR_BGR2GRAY)
+            grayPre = cv2.cvtColor(self.imgPre, cv2.COLOR_BGR2GRAY)
 
-    #imgPre= cv2.flip(imgPre,1)
-    #imgInput=cv2.flip(imgInput,1)
-    imgPre=cv2.resize(imgPre, (680,480))
-    imgInput=cv2.resize(imgInput, (680,480))
 
-    rows, cols, channels = imgPre.shape
+        gauInput = cv2.GaussianBlur(grayInput, (5, 5), 0)
+        gauPre= cv2.GaussianBlur(grayPre, (5, 5), 0)
 
-    Line = np.zeros((2, 2), np.float32)
-    linePosition = round(cols * 0.75)
+        imgDiff= cv2.absdiff(gauInput,gauPre)
 
-    Line[0][0] = linePosition
-    Line[1][0] = linePosition
-    Line[0][1] = 0
-    Line[1][1] = cols
+        # 이진화
+        _, imgBinary = cv2.threshold(imgDiff, 30, 200, cv2.THRESH_BINARY)
 
-#cv2.imshow('imgPre', imgPre)
+        # 모폴로지 연산을 위한 mask 설정
+        mask = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
 
-    preCopy=imgPre
-    inputCopy=imgInput
+        # 침식,팽창을 이용한 전처리
+        for i in range(0, 1):
+            imgBinary = cv2.dilate(imgBinary, mask, iterations=1)
+            imgBinary = cv2.dilate(imgBinary, mask, iterations=1)
+            imgBinary = cv2.erode(imgBinary, mask, iterations=1)
 
-## 이미지 그레이스케일로 변경
-    if _ is True:
-        inputCopy = cv2.cvtColor(inputCopy ,cv2.COLOR_BGR2GRAY)
-        preCopy= cv2.cvtColor(preCopy, cv2.COLOR_BGR2GRAY)
-    else:
-        continue
+            # 윤곽선 따기
+        image, contours, hierarchy = cv2.findContours(imgBinary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        imgContour = cv2.drawContours(imgBinary, contours, -1, SCALAR_WHITE, -1)
 
-## 가우시안 필터를 이용한 전처리
-    inputCopy= cv2.GaussianBlur( inputCopy, (5, 5), 0)
-    preCopy = cv2.GaussianBlur( preCopy, (5, 5), 0)
+        # 윤곽선 뭉뚱그리기? 아무튼 그런비슷한 개념이었음 윤곽선을 단순화하는 느낌
+        global convexHulls
+        convexHulls = []
 
-    imgDiff =cv2.absdiff(inputCopy,preCopy)
+        for i in range(len(contours)):
+            convexHull = cv2.convexHull(contours[i])
+            convexHulls.append(convexHull)
 
-    #cv2.imshow('diff',imgDiff)
+            # 어떤 조건에 취합한 blob을 인식할 것인지 조건문
+        frameBlob = []
+        for i in range(len(convexHulls)):
+            checkBlob = blob(convexHulls[i])
 
-# 이진화
-    _, imgBinary = cv2.threshold(imgDiff,30,200,cv2.THRESH_BINARY)
+            if (checkBlob.diagonal > 60 and checkBlob.boundingRect[2] > 150 and
+                        checkBlob.boundingRect[3] < 250 and checkBlob.aspectRatio < 1.5 and checkBlob.boundingRect[
+                2] < 300):
+                frameBlob.append(checkBlob)
+        # 첫번째 프레임이면 blob정보 그대로 넣고 첫번째 프레임이 아니라면 match 함수로
 
-# 모폴로지 연산을 위한 mask 설정
-    mask= cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
+        if (self.firstFrame == True):
+            for i in frameBlob:
+                global blobs
 
-# 침식,팽창을 이용한 전처리
-    for i in range(0, 1):
-        imgBinary=cv2.dilate(imgBinary,mask,iterations = 1)
-        imgBinary=cv2.dilate(imgBinary,mask,iterations = 1)
-        imgBinary=cv2.erode(imgBinary,mask, iterations = 1)
+                blobs.append(i)
 
-#윤곽선 따기
-    image, contours ,hierarchy=cv2.findContours(imgBinary,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    imgContour =cv2.drawContours(imgBinary,contours,-1, SCALAR_WHITE, -1)
+        else:
+            blobs = matchCurrentFrameBlobsToExistingBlobs(blobs, frameBlob)
 
-    #cv2.imshow("contours", imgContour)
+        inputCopy = self.imgInput
 
-#윤곽선 뭉뚱그리기? 아무튼 그런비슷한 개념이었음 윤곽선을 단순화하는 느낌
-    global convexHulls
-    convexHulls = []
+        drawBlobInfoOnImage(blobs, inputCopy)
 
-    for i in range(len(contours)):
-        convexHull = cv2.convexHull(contours[i])
-        convexHulls.append(convexHull)
+        # 객체가 라인을 통과했는지 확인
+        self.passLine = checkIfBlobsCrossedTheLine(blobs, self.linePosition, mortarCount)
 
-# 어떤 조건에 취합한 blob을 인식할 것인지 조건문
-    frameBlob = []
-    for i in range(len(convexHulls)):
-        checkBlob = blob(convexHulls[i])
 
-        if (checkBlob.diagonal > 60 and checkBlob.boundingRect[2] > 150 and
-                    checkBlob.boundingRect[3] < 250 and checkBlob.aspectRatio < 1.5 and checkBlob.boundingRect[2] <300 ) :
-            frameBlob.append(checkBlob)
 
- # 첫번째 프레임이면 blob정보 그대로 넣고 첫번째 프레임이 아니라면 match 함수로
+        # blob이 관심영역으로 지정한 linePosition을 지나는 순간 해당영역 이미지 객체 분류
+        if (self.passLine == True):
+            cv2.line(inputCopy, (self.Line[0][0], self.Line[0][1]), (self.Line[1][0], self.Line[1][1]), SCALAR_GREEN, 2)
 
 
-    if (firstFrame == True):
-        for i in frameBlob:
-            blobs.append(i)
-    else:
-        blobs = matchCurrentFrameBlobsToExistingBlobs(blobs, frameBlob)
+             # linePosition 영역만 저장
+            imgTest = self.imgPre[30:800, self.linePosition - 700:self.linePosition + 700]
+            i = i + 10
 
-    inputCopy = imgInput
 
-    drawBlobInfoOnImage(blobs, inputCopy)
+            cv2.imwrite("hymTest" + str(i) + ".jpg", imgTest)
+            image_expanded = np.expand_dims(imgTest, axis=0)
 
-    passLine = checkIfBlobsCrossedTheLine(blobs,linePosition,mortarCount)
 
+            # 몰탈 포장 분류 object_detection API
 
-    #score, classes = objectDetection.mortarClassification(imgInput, image_expanded)
+            score, classes = objectDetection.mortarClassification(imgTest, image_expanded)
 
+            try:
+                # npy 형변화 필요 없을시 error
+                imgTest = np.transpose(imgTest, (0, 1, 2)).copy()
+                # 분류 화면 뿌려줌
+                self.displayImage(imgTest, 2)
 
-    # blob이 관심영역으로 지정한 linePosition을 지나는 순간 해당영역 이미지 객체 분류
-    if(passLine == True):
-        cv2.line(inputCopy, (Line[0][0], Line[0][1]), (Line[1][0], Line[1][1]), SCALAR_GREEN, 3)
+            except Exception as ex:
+                print(ex)
 
-        # linePosition 영역만 저장
-        imgTest = imgPre[30:800,linePosition-700:linePosition+700]
-        i= i+10
+            ret = 2
+            #cv2.imshow("classfication", imgTest)
 
-        cv2.imwrite("hymTest"+str(i)+".jpg",imgTest)
-        image_expanded = np.expand_dims(imgTest, axis=0)
 
-        # 몰탈 포장 분류 object_detection API
+            # 몰탈 카운트 함수
+            mortarClassificationAndCount(classes, mortarCount)
 
-        score,classes=objectDetection.mortarClassification(imgTest,image_expanded)
+            # result[0] = score result[1]= class
+            #print(score, classes)
 
-        ret = 2
-        cv2.imshow("classfication",imgTest)
 
-        #몰탈 카운트 함수
-        mortarClassificationAndCount(classes,mortarCount)
+        else:
+            cv2.line(inputCopy, (self.Line[0][0], self.Line[0][1]), (self.Line[1][0], self.Line[1][1]), SCALAR_RED, 2)
 
-        #result[0] = score result[1]= class
-        print(score,classes)
+        # 몰탈 카운트 정보 영상에 출력
+        #drawMortarCountOnImage(mortarCount, inputCopy)
 
+        self.mortarCounter()
 
-    else:
-        cv2.line(inputCopy, (Line[0][0], Line[0][1]), (Line[1][0], Line[1][1]), SCALAR_RED, 3)
+        #직접적인 Qt Ui에 이미지 뿌리기
+        self.displayImage(inputCopy,1)
 
-    # 몰탈 카운트 정보 영상에 출력
-    drawMortarCountOnImage(mortarCount,inputCopy)
 
-    cv2.imshow('result',inputCopy)
 
-  #  if ((cap.get(cv2.CAP_PROP_POS_FRAMES) + 1) < cap.get(cv2.CAP_PROP_FRAME_COUNT)):
-  #      cap.read(imgInput)
+        # 9분마다 몰탈 포장 갯수 temp에 일시 저장
+        if(now.tm_min%10-9 ==0 and now.tm_sec ==0):
+            for i in range(0,6):
+                mortarTemp[i] = mortarCount[i]
 
-  #  else :
-  #      print("end")
+        # 8분마다 몰탈 포장 갯수 DB에 업데이트 단 8분에는 ptime -1 에 해당되는 영역 select 해야함.
 
-  #      break
+        if(now.tm_min%10-8 ==0 and now.tm_sec ==30 ):
 
-    # 9분마다 몰탈 포장 갯수 temp에 일시 저장
-    if(now.tm_min%10-9 ==0 and now.tm_sec ==0):
-        for i in range(0,6):
-            mortarTemp[i] = mortarCount[i]
+            cursor = conn.cursor()
+            cursor.execute(
+                "select * from PCS_PRODUCT_COUNT where PLC_TIME = %s and ITEM_CD = %s and BPNO= 'A' " ,(plcTime,classesTemp))
 
-    # 8분마다 몰탈 포장 갯수 DB에 업데이트 단 8분에는 ptime -1 에 해당되는 영역 select 해야함.
+            data= cursor.fetchall()
+            print (len(data))
+            print (data)
 
-    if(now.tm_min%10-8 ==0 and now.tm_sec ==30 ):
+            # select 된 갯수가 12개 존재 할 때 update 시작 (오류 방지)
+            if(len(data)>= 1 ) :
 
-        cursor = conn.cursor()
-        cursor.execute(
-            "select * from PCS_PRODUCT_COUNT where PLC_TIME = %s and ITEM_CD = %s and BPNO= 'A' " ,(plcTime,classesTemp))
+                cursor2 = conn.cursor()
 
-        data= cursor.fetchall()
-        print (len(data))
-        print (data)
 
-        # select 된 갯수가 12개 존재 할 때 update 시작 (오류 방지)
-        if(len(data)>= 1 ) :
+                cursor2.execute("update PCS_PRODUCT_COUNT set IMG_CNT= %s  where PLC_TIME= %s and BPNO = 'A' ",(int (mortarTemp[0]), plcTime))
+                conn.commit()
 
-            cursor2 = conn.cursor()
 
-            cursor2.execute("update PCS_PRODUCT_COUNT set IMG_CNT= %s  where PLC_TIME= %s and BPNO = 'A' ",(int (mortarTemp[0]), plcTime))
-            conn.commit()
+                print(plcTime)
 
-            print(int(mortarTemp[0]))
-            print(plcTime)
+            # 8분마다 DB 업데이트 후 몰탈 TEMP 초기화
+            for i in range(0, 6):
+                mortarTemp[i] = 0
 
-        # 8분마다 DB 업데이트 후 몰탈 TEMP 초기화
-        for i in range(0, 6):
-            mortarTemp[i] = 0
+        # 00시 정각에 모든 몰탈 카운트 정보 초기화
+        if(now.tm_hour==0 and now.tm_min == 0 and now.tm_sec == 0 ):
 
-    # 00시 정각에 모든 몰탈 카운트 정보 초기화
-    if(now.tm_hour==0 and now.tm_min == 0 and now.tm_sec == 0 ):
+            for i in range(0,6):
+              mortarTemp[i] = mortarCount[i]
+              mortarCount[i]= 0
 
-        for i in range(0,6):
-          mortarTemp[i] = mortarCount[i]
-          mortarCount[i]= 0
 
-    # blob clear
-    imgPre = imgInput
-    frameBlob.clear()
+        self.imgPre = self.imgInput
+        frameBlob.clear()
+        self.firstFrame = False
 
-    firstFrame = False
-    cv2.waitKey(10)
+####################################################################################
+# 실질적으로 Opencv Mat 형 이미지를 Qt형 이미지로 변환후 display
+    def displayImage(self,img,window=1):
 
-    #  esc 누르면 종료
-    if cv2.waitKey(1) ==27:
-        break
-'''
-    cursor = conn.cursor()
-    cursor.execute(
-        "select * from PCS_PRODUCT_COUNT where PDATE=%s and PTIME = %s ", (ndate, now.tm_hour))
+        qformat=QImage.Format_Indexed8
 
-    data = cursor.fetchall()
-    print(len(data))
-'''
+        if len(img.shape)==3:
+            if(img.shape[2])==4:
+                qformat=QImage.Format_RGBA8888
+            else:
+                qformat=QImage.Format_RGB888
+        outImage=QImage(img,img.shape[1],img.shape[0],img.strides[0],qformat)
 
-cap.release()
-cv2.destroyAllWindows()
+        outImage= outImage.rgbSwapped()
 
-cursor.close()
-cursor2.close()
-conn.close()
+        if window ==1 :
+            self.imgLabel.setPixmap(QPixmap.fromImage(outImage))
+            self.imgLabel.setScaledContents(True)
+        if window ==2 :
+
+            self.imgLabel2.setPixmap(QPixmap.fromImage(outImage))
+            self.imgLabel2.setScaledContents(True)
+
+
+
+            # self.imgLabel.setAlignmnt(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
+
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    myWindow = MyWindow()
+    myWindow.show()
+    app.exec_()
